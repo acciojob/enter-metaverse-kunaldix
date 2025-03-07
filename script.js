e802d4be594a22f4b4a7a70c4bd2134c2b65a70a
Kunal Dixit <kunaldixit15049@gmail.com>
2025-03-07 19:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const metaElement = document.querySelector("#status");
+const btn = document.querySelector("#enterBtn");
+
+btn.addEventListener ('click', () => {
+	metaElement.innerHTML = '<h1> Entered Metaverse </h1>'
+});
